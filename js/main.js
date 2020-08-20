2d2ffa362a2b65fe3e20af23de75ddd631d9acf7
ledimari <ledi.huliganka@gmail.com>
2020-08-20 20:06:25 +0300

    hotel desctop version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-function init() {
-  // функция вызовет сама себя как только сайт загрузиться
-}
+$(function(){
+  $('.slider').slick({
+    nextArrow: '<button class="slick-arrow slick-next"><img src="img/next.svg" alt="next arrow"/></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="img/prev.svg" alt="prev arrow"/></button>',
+    fade: true
+  });
+});
 
-(init)();
+
+
+
+
+
+
